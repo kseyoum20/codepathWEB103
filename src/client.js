e33aff8a7bd6928ee2837cde085210da109b74b3
kseyoum20 <120141948+kseyoum20@users.noreply.github.com>
2023-08-05 02:35:05 -0400

    Initial commit

---
+++ src/client.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+const URL = 'https://higfewykteutelavxfyd.supabase.co';
+const API_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZ2Zld3lrdGV1dGVsYXZ4ZnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyMDM4MjYsImV4cCI6MjAwNjc3OTgyNn0.l9-4fGRwmH4gHKkUOzMGHsYAEGJ1eUUL0syombPie7A';
+const supabase = createClient(URL, API_KEY);

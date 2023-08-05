24c0e5ecccea5572cf51da4bfc2381b6f903becf
kseyoum20 <120141948+kseyoum20@users.noreply.github.com>
2023-08-05 02:36:22 -0400

    1 file modified

--- src/client.js
+++ src/client.js
@@ -3,3 +3,4 @@ const URL = 'https://higfewykteutelavxfyd.supabase.co';
 const API_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZ2Zld3lrdGV1dGVsYXZ4ZnlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyMDM4MjYsImV4cCI6MjAwNjc3OTgyNn0.l9-4fGRwmH4gHKkUOzMGHsYAEGJ1eUUL0syombPie7A';
 const supabase = createClient(URL, API_KEY);
+export const supabase = createClient(URL, API_KEY);
